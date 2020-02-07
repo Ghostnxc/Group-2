2612d7f753cfcf4467aae9a9c85d02fff43faef7
JAB93 <56855296+JAB93@users.noreply.github.com>
2020-02-07 16:13:08 -0500

    Summary

---
+++ Chapter2Project/src/chapter2project/Chapter2Project.java
@@ -0,0 +1,15 @@
+package chapter2project;
+import java.util.Scanner;
+
+public class Chapter2Project {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        Scanner inputDevice = new Scanner(System.in);
+        System.out.println("Please enter your name >> ");
+        name = inputDevice.nextLine();
+        System.out.println("Please enter your age >> ");
+        age = inputDevice.nextInt();
+        System.out.println("Your name is " + name + " and your age is " + age + " years old.");
+    }
+}
